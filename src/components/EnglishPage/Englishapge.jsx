9c13904ed50b158562dd515310dac011c286ddc8
Ahmed Anwer <qqwweerrtt01126989864@gmail.com>
2024-11-21 00:23:33 +0200

    updates english page

--- src/components/EnglishPage/Englishapge.jsx
+++ src/components/EnglishPage/Englishapge.jsx
@@ -3,12 +3,23 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "./EnglishPage.css";
-import img1 from "../../assets/11.jpeg";
+import img1 from "../../assets/EnglishBG3.jpg";
 import img2 from "../../assets/1.jpg";
 import img3 from "../../assets/12.jpeg";
+import img4 from "../../assets/EnglishBG2.jpg";
+import img5 from "../../assets/EnglishBG.jpg";
 import videos from "../../assets/background.mp4";
+import NavEnglish from "./NavEnglish";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
 const responsive = {
   superLargeDesktop: { breakpoint: { max: 4000, min: 1024 }, items: 3 },
   desktop: { breakpoint: { max: 1024, min: 768 }, items: 2 },
@@ -18,46 +29,121 @@ const responsive = {
 
 const servicesData = [
   {
-    title: "Feasibility Studies for Projects",
-    text: "Comprehensive support to prepare and develop feasibility studies for charity projects that align with donor requirements.",
+    title: "Feasibility Studies Preparation",
+    text: {
+      english: "Comprehensive support to prepare and develop feasibility studies for charity projects that align with donor requirements.",
+    },
+
     image: img1,
   },
   {
-    title: "Project Reporting Services",
-    text: "Provide periodic and final project reports according to donor standards to enhance credibility and funding continuity.",
+    title: "Project Reporting",
+    text: {
+      english: "Provide periodic and final project reports according to donor standards to enhance credibility and funding continuity.",
+    },
     image: img2,
   },
   {
     title: "Grant Application Support",
-    text: "Assistance in preparing and submitting grant requests, including guidance on proposal writing and follow-ups.",
+    text: {
+      english: "Assistance in preparing and submitting grant requests, including guidance on proposal writing and follow-ups.",
+    },
     image: img3,
+  },
+  {
+    title: "Registration on Arab Grant Platforms",
+    text: {
+      english: "Offering registration services and guidance on how to use Arab grant platforms to access available support opportunities.",
+    },
+    image: img4, // You can use the appropriate image for this service
+  },
+  {
+    title: "Resource Development Consulting",
+    text: {
+      english: "Providing advice on the best methods for resource development in humanitarian projects and enhancing their sustainability by connecting with donors and marketing the projects.",
+    },
+    image: img5, // You can use the appropriate image for this service
   },
 ];
 
+
 const EnglishPage = () => {
+  function DrawerExample() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const btnRef = React.useRef();
+
+    return (
+      <>
+        <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+          Open
+        </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Create your account</DrawerHeader>
+
+            <DrawerBody>
+              <Input placeholder="Type here..." />
+            </DrawerBody>
+
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="blue">Save</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </>
+    );
+  }
   return (
     <div className="english-page">
-      {/* Navbar */}
-     
-<nav className="navbar">
-  <Container>
-    <ul className="navbar-list">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/services">Services</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
-    </ul>
-  </Container>
-</nav>
+      <section className="header-slider">
+        <Carousel
+          responsive={responsive}
+          infinite
+          autoPlay
+          autoPlaySpeed={3000}
+          showDots
+        >
+          <img src={img4} alt="Header 1" className="slider-image" />
+          <img src={img5} alt="Header 2" className="slider-image" />
+          <img src={img1} alt="Header 3" className="slider-image" />
+        </Carousel>
+      </section>
 
-      {/* Banner */}
-      <div className="banner">
-        <div className="banner-content">
-          <h1>Empowering Projects Across the Arab World</h1>
-          <Button variant="light" className="banner-btn">
-            Explore Services
-          </Button>
-        </div>
-      </div>
+      {/* About Us Section */}
+      <section className="about-us-section">
+        <Container>
+          <h2 className="section-title">About Us</h2>
+          <Row>
+            <Col md={6}>
+              <p className="text-aboutus">
+                Esteshara Platform We offer you support and guidance to develop
+                your projects and attract funding for your humanitarian and
+                charitable initiatives. We are your strategic partner that
+                empowers you to reach donors and funding platforms in the Arab
+                world.
+              </p>
+            </Col>
+            <Col md={6}>
+              <div className="video-container">
+                <video className="img-fluid" autoPlay muted loop>
+                  <source src={videos} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
 
       {/* Services Section */}
       <section className="services-section">
@@ -65,10 +151,10 @@ const EnglishPage = () => {
           <h2 className="section-title">Our Premium Services</h2>
           <Carousel
             responsive={responsive}
-            infinite={true}
-            autoPlay={true}
+            infinite
+            autoPlay
             autoPlaySpeed={3000}
-            showDots={true}
+            showDots
           >
             {servicesData.map((service, index) => (
               <div key={index} className="service-card">
@@ -79,7 +165,9 @@ const EnglishPage = () => {
                 />
                 <div className="service-content">
                   <h3 className="service-title">{service.title}</h3>
-                  <p className="service-text">{service.text}</p>
+                  <p className="service-text">{service.text.english}</p>
+                 
+
                 </div>
               </div>
             ))}
@@ -94,29 +182,77 @@ const EnglishPage = () => {
           <Row>
             <Col md={6}>
               <ul className="why-choose-list">
-                <li>Decades of experience with donor cultures.</li>
-                <li>Fast and reliable access to Arab donors.</li>
-                <li>Comprehensive consultations tailored to funding.</li>
-                <li>Support from ideation to execution.</li>
+                <li>Deep Understanding of Donor Culture and Standards </li>
+                <li>Quick and Direct Access to Arab Donor Networks.</li>
+                <li>Specialized and Targeted Consultation.</li>
+                <li>Comprehensive Support from Concept to Execution.</li>
               </ul>
             </Col>
             <Col md={6}>
-              <video className="img-fluid" autoplay muted>
-                <source src={videos} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+              <img src={img3} alt="Why Choose Us" className="img-fluid" />
             </Col>
           </Row>
         </Container>
       </section>
 
-      {/* Call to Action */}
-      <div className="cta-section">
-        <h3>Let's Collaborate on Your Next Big Idea</h3>
-        <Button variant="light" size="lg">
-          Get Started
-        </Button>
-      </div>
+      {/* Benefits Section */}
+      <section className="benefits-section">
+        <Container>
+          <h2 className="section-title">Key Benefits</h2>
+          <Row>
+            <Col md={3}>
+              <div className="benefit-card">
+             
+                <p>
+                Facilitating the submission of your projects in a professional format that aligns with the culture and requirements of donors.            
+                </p>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="benefit-card">
+               
+                <p>
+                Enabling direct access to donor platforms and suitable funding opportunities.                </p>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="benefit-card">
+                
+                <p>
+                Enhancing the credibility of your projects and increasing the chances of continuous funding through well-prepared reports.                </p>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="benefit-card">
+             
+                <p>
+                Growing your resources and achieving sustainability for your charitable initiatives.
+
+                    </p>
+              </div>
+            </Col>  
+          </Row>
+        </Container>
+      </section>
+      <footer className="contact-footer">
+      <Container>
+        <Row className="justify-content-center text-center">
+          <Col md={8}>
+            <h3 className="footer-title">
+              Let Us Help You Bring Your Project to Life in the Arab World.
+            </h3>
+            <p className="footer-description">
+              Connect with us today to start your journey and achieve success in your charitable initiatives.
+            </p>
+            <Link to="/contact">
+            <Button variant="primary" href="#contact-form" className="contact-btn">
+              Contact Us
+            </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
     </div>
   );
 };
